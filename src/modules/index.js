2b18ba0073de8cc57be59a37fae842ae061d96ca
Guilherme Santos <guilherme.santos@conexasaude.com.br>
2021-10-24 11:22:41 -0300

    fixing npm exporting components

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,5 @@
+import React from 'react'
 import SignInPageComponent from "./signin/components/SignInPage";
 import SignUpPageComponent from "./signup/components/SignUpPage";
-
 export const SignInPage = (props) => <SignInPageComponent {...props} />;
 export const SignUpPage = (props) => <SignUpPageComponent {...props} />;
